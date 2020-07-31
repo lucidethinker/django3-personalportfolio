1ae7c9f75da6bd591621f89a13de40c936a60f01
lucidethinkers <lucidethinkers@gmail.com>
2020-07-31 22:30:09 +0530

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+
+# from django.conf.urls import include, url
+from django.urls import path
+from blog import views
+
+app_name = 'blog'
+urlpatterns = [
+    path('',views.all_blogs,name='all_blogs'),
+    path('<int:blog_id>/',views.detail,name='detail'),
+]
---
+++ portfolio/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Project
+
+
+def home(request):
+	projects = Project.objects.all()
+	return render(request,'portfolio/home.html', {'projects':projects})
+def son(request):
+	return request(request,'portfolio/soon.html', {})
+
+
+
+
+
